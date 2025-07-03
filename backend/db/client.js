190d9698deb83be03ead296e02b2bcf445a5d02e
Kai Wui Ye <kye5595@gmail.com>
2025-07-02 21:20:42 -0400

    env updates

--- backend/db/client.js
+++ backend/db/client.js
@@ -1,11 +1,14 @@
 import { Pool } from "pg";
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: parseInt(process.env.DB_PORT || "5432", 10),
+  ssl: { rejectUnauthorized: false },
 });
+
 // Test connection and log any errors during pool creation or connection
 pool.on("connect", () => {
   console.log("Successfully connected to the PostgreSQL database pool");
